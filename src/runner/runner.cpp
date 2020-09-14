c11830894f08743da2f89cc78d61dd745027b935
momo5502 <mauriceheumann@gmail.com>
2020-09-14 22:04:38 +0200

    Cleanup runner

--- src/runner/runner.cpp
+++ src/runner/runner.cpp
@@ -2,30 +2,22 @@
 #include <Windows.h>
 #include <cstdlib>
 
-namespace
-{
-	void run_watchdog()
-	{
-		auto* const command = "-proc ";
-		const char* parent_proc = strstr(GetCommandLineA(), command);
-
-		if (parent_proc)
-		{
-			const auto pid = atoi(parent_proc + strlen(command));
-			auto* const process_handle = OpenProcess(SYNCHRONIZE, FALSE, pid);
-			if (process_handle && process_handle != INVALID_HANDLE_VALUE)
-			{
-				WaitForSingleObject(process_handle, INFINITE);
-				CloseHandle(process_handle);
-			}
-
-			TerminateProcess(GetCurrentProcess(), 0);
-		}
-	}
-}
-
 int __stdcall WinMain(HINSTANCE, HINSTANCE, PSTR, int)
 {
-	run_watchdog();
-	return 0;
+	auto* const command = "-proc ";
+	const char* parent_proc = strstr(GetCommandLineA(), command);
+
+	if (parent_proc)
+	{
+		const auto pid = atoi(parent_proc + strlen(command));
+		auto* const process_handle = OpenProcess(SYNCHRONIZE, FALSE, DWORD(pid));
+		if (process_handle)
+		{
+			WaitForSingleObject(process_handle, INFINITE);
+			CloseHandle(process_handle);
+			return 0;
+		}
+	}
+
+	return 1;
 }
