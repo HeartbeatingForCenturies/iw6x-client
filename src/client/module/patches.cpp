ed568f33216b46aea973e62ba1c068d3e540761a
FragsAreUs <32180105+FragsAreUs@users.noreply.github.com>
2020-09-19 14:48:42 -0400

    [patch] added extinction stuff

--- src/client/module/patches.cpp
+++ src/client/module/patches.cpp
@@ -82,9 +82,14 @@ public:
 		// add Unlock all Command - only does level for squad member 1 if people want level 60 on all squad members let me know and I can just add the rest of the squad members
 		command::add("unlockall", [](command::params&)
 		{
-			utils::hook::set<BYTE>(0x1445A3798, 0x0A);
-			utils::hook::set<float>(0x1445A34A0, 50000); 
-			utils::hook::set<short>(0x14459F857, 4805);
+			//only Mp
+			utils::hook::set<BYTE>(0x1445A3798, 0x0A);	// Prestige
+			utils::hook::set<float>(0x1445A34A0, 50000); // squad points
+			utils::hook::set<short>(0x14459F857, 4805);	// squad member 1 level 
+			//only Extinction
+			utils::hook::set<short>(0x1445A6B62, 9999);	// Teeth
+			utils::hook::set<BYTE>(0x1445A5F96, 25);	// Prestige
+			utils::hook::set<short>(0x1445A5F90, 27);	// level
 		});
 		
 		// add quit command
