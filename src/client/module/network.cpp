e0cec8bb086f222d4911f8694687e512ac8035df
momo5502 <mauriceheumann@gmail.com>
2020-09-21 22:33:18 +0200

    More DTLS killing + partial connect command

--- src/client/module/network.cpp
+++ src/client/module/network.cpp
@@ -52,6 +52,48 @@ namespace
 		a.popad64();
 		a.jmp(0x1402C64EE);
 	}
+
+	enum class compare_result : std::int32_t
+	{
+		equal = 1,
+		not_equal = 0,
+	};
+
+	compare_result net_compare_base_address(game::native::netadr_s* a1, game::native::netadr_s* a2)
+	{
+		if (a1->type == a2->type)
+		{
+			switch (a1->type)
+			{
+			case game::native::netadrtype_t::NA_BOT:
+			case game::native::netadrtype_t::NA_LOOPBACK:
+				return (a1->port == a2->port) ? compare_result::equal : compare_result::not_equal;
+
+			case game::native::netadrtype_t::NA_IP:
+				return (!memcmp(a1->ip, a2->ip, 4)) ? compare_result::equal : compare_result::not_equal;
+			case game::native::netadrtype_t::NA_BROADCAST:
+				return compare_result::equal;
+			default:
+				break;
+			}
+		}
+
+		return compare_result::not_equal;
+	}
+
+	compare_result net_compare_address(game::native::netadr_s* a1, game::native::netadr_s* a2)
+	{
+		if (net_compare_base_address(a1, a2) == compare_result::equal)
+		{
+			if (a1->port == a2->port)
+			{
+				return compare_result::equal;
+			}
+		}
+
+		// not equals
+		return compare_result::not_equal;
+	}
 }
 
 void network::on(const std::string& command, const callback& callback)
@@ -82,6 +124,23 @@ void network::post_unpack()
 	// intercept command handling
 	utils::hook::jump(0x1402C64CB, utils::hook::assemble(handle_command_stub), true);
 
+	utils::hook::jump(0x14041D010, net_compare_address);
+	utils::hook::jump(0x14041D060, net_compare_base_address);
+
+	// don't establish secure conenction
+	utils::hook::set<uint8_t>(0x1402ECF1D, 0xEB);
+	utils::hook::set<uint8_t>(0x1402ED02A, 0xEB);
+	utils::hook::set<uint8_t>(0x1402ED34D, 0xEB);
+	utils::hook::set<uint8_t>(0x1402C4A1F, 0xEB); //
+
+	// ignore unregistered connection
+	utils::hook::jump(0x140471AAC, reinterpret_cast<void*>(0x140471A50));
+	utils::hook::set<uint8_t>(0x140471AF0, 0xEB);
+
+	// disable xuid verification
+	utils::hook::set<uint8_t>(0x140154AA9, 0xEB);
+	utils::hook::set<uint8_t>(0x140154AC3, 0xEB);
+
 	command::add("lul", [](command::params&)
 	{
 		game::native::netadr_s addr{};
@@ -92,10 +151,26 @@ void network::post_unpack()
 		network::send(addr, "test_command", "this is a test");
 	});
 
+	command::add("hehe", [](command::params&)
+	{
+		char session_info[0x1000] = {};
+
+		game::native::netadr_s addr{};
+		addr.port = htons(27016);
+		addr.type = game::native::NA_IP;
+		inet_pton(AF_INET, "192.168.10.19", addr.ip);
+
+		// CL_ConnectFromParty
+		((void(*)(int, char*, game::native::netadr_s*, const char*, const char*))0x1402CB6D0)(
+			0, session_info, &addr, "mp_prisonbreak", "war");
+	});
+
 	network::on("test_command", [](const game::native::netadr_s& address, const std::string_view& data)
 	{
 		printf("YEEEEEEEEEEES: %s\n", std::string(data).data());
 	});
 }
 
+#if defined(DEV_BUILD)
 REGISTER_MODULE(network)
+#endif
