966a0cf198ecf178c735fcd30862ba8ceb9c50bb
Jimbo <49079230+Jemal@users.noreply.github.com>
2020-10-25 15:45:05 +0100

    added iw6x version to the games version strings

--- src/client/module/branding.cpp
+++ src/client/module/branding.cpp
@@ -4,8 +4,28 @@
 #include "scheduler.hpp"
 #include "game/game.hpp"
 
+#include "utils/hook.hpp"
+#include "utils/string.hpp"
+
 namespace branding
 {
+	namespace
+	{
+		utils::hook::detour ui_get_formatted_build_number_hook;
+		
+		void dvar_set_string_stub(game::dvar_t* dvar, const char* string)
+		{
+			game::Dvar_SetString(dvar, utils::string::va("IW6x %s (game %s)", VERSION, string));
+		}
+
+		const char* ui_get_formatted_build_number_stub()
+		{
+			const auto* const build_num = ui_get_formatted_build_number_hook.invoke<const char*>();
+			
+			return utils::string::va("%s (%s)", VERSION, build_num);
+		}
+	}
+	
 	class module final : public module_interface
 	{
 	public:
@@ -22,6 +42,9 @@ namespace branding
 			}
 
 			localized_strings::override("LUA_MENU_LEGAL_COPYRIGHT", "IW6x: Pre-Release by X Labs.\n");
+
+			utils::hook::call(SELECT_VALUE(0x1403BDABA, 0x140414424), dvar_set_string_stub);
+			ui_get_formatted_build_number_hook.create(SELECT_VALUE(0x140415FD0, 0x1404D7C00), ui_get_formatted_build_number_stub);
 
 			scheduler::loop([]()
 			{
