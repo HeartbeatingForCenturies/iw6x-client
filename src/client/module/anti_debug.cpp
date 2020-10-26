0927d17be5f391e465ca031d836cc79cb170630a
momo5502 <mauriceheumann@gmail.com>
2020-10-26 23:12:38 +0100

    Unfinished arxan performance optimizations

--- src/client/module/anti_debug.cpp
+++ src/client/module/anti_debug.cpp
@@ -2,6 +2,7 @@
 #include "loader/module_loader.hpp"
 #include "scheduler.hpp"
 #include "utils/hook.hpp"
+#include "game/game.hpp"
 
 namespace anti_debug
 {
@@ -81,6 +82,49 @@ namespace anti_debug
 			peb->BeingDebugged = false;
 			*PDWORD(LPSTR(peb) + 0xBC) &= ~0x70;
 		}
+
+		volatile bool allow_lobby_pump = true;
+		utils::hook::detour lobby_pump_hook;
+
+		void lobby_pump_stub(const int controller_index)
+		{
+			if (allow_lobby_pump)
+			{
+				allow_lobby_pump = false;
+				lobby_pump_hook.invoke<void*>(controller_index);
+			}
+		}
+
+		volatile bool allow_net_pump = true;
+		utils::hook::detour net_pump_hook;
+
+		void net_pump_stub(const int controller_index)
+		{
+			if (allow_net_pump)
+			{
+				allow_net_pump = false;
+				net_pump_hook.invoke<void*>(controller_index);
+			}
+		}
+
+		game::DWOnlineStatus get_logon_status_stub(const int controller_index)
+		{
+			static auto is_connected = false;
+			if (is_connected)
+			{
+				return game::DW_LIVE_CONNECTED;
+			}
+
+			const auto result = reinterpret_cast<game::DWOnlineStatus(*)(int)>(0x1405894C0)(controller_index);
+			if (result == game::DW_LIVE_CONNECTED)
+			{
+				is_connected = true;
+				lobby_pump_hook.create(0x140591850, lobby_pump_stub);
+				net_pump_hook.create(0x140558C20, net_pump_stub);
+			}
+
+			return result;
+		}
 	}
 
 	class module final : public module_interface
@@ -99,6 +143,23 @@ namespace anti_debug
 
 			AddVectoredExceptionHandler(1, exception_filter);
 		}
+
+		/*void post_unpack() override
+		{
+			// cba to implement sp, not sure if it's even needed
+			if (game::environment::is_sp()) return;
+
+			scheduler::loop([]
+			{
+				allow_lobby_pump = true;
+				allow_net_pump = true;
+			}, scheduler::pipeline::async, 300ms);
+
+			// Some arxan exception stuff to obfuscate functions
+			// This is causing lags
+			// We will have to properly patch that some day
+			utils::hook::jump(0x140589480, get_logon_status_stub);
+		}*/
 	};
 }
 
