27e94d6b67b59a43c85155185919caf6a257f405
Federico Cecchetto <fedecek3@gmail.com>
2020-12-08 17:31:09 +0100

    Hook command handler

--- src/client/component/logfile.cpp
+++ src/client/component/logfile.cpp
@@ -4,34 +4,46 @@
 #include "game/scripting/entity.hpp"
 #include "game/scripting/execution.hpp"
 
+#include <utils/hook.hpp>
+
 #include "command.hpp"
 
 namespace logfile
 {
+	namespace
+	{
+		utils::hook::detour client_command_hook;
+
+		void client_command_stub(const int clientNum, void* a2)
+		{
+			command::params_sv params = {};
+			const auto cmd = params.get(0);
+
+			if (cmd == "say"s || cmd == "say_team"s)
+			{
+				const auto player = scripting::call("getEntByNum", {clientNum}).as<scripting::entity>();
+
+				auto msg = params.join(1);
+				msg.erase(0, 1);
+
+				scripting::notify(player, cmd, {msg});
+			}
+
+			client_command_hook.invoke<void>(clientNum, a2);
+		}
+	}
+
 	class component final : public component_interface
 	{
 	public:
 		void post_unpack() override
 		{
-			command::add_sv("say", [](const int clientNum, const command::params_sv& params)
+			if (game::environment::is_sp())
 			{
-				const auto player = scripting::call("getEntByNum", {clientNum}).as<scripting::entity>();
+				return;
+			}
 
-				auto msg = params.join(1);
-				msg.erase(0, 1);
-
-				scripting::notify(player, "say", {msg});
-			});
-
-			command::add_sv("say_team", [](const int clientNum, const command::params_sv& params)
-			{
-				const auto player = scripting::call("getEntByNum", {clientNum}).as<scripting::entity>();
-
-				auto msg = params.join(1);
-				msg.erase(0, 1);
-
-				scripting::notify(player, "say_team", {msg});
-			});
+			client_command_hook.create(0x1403929B6, client_command_stub);
 		}
 	};
 }
