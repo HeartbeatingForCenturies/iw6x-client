3f2a97205dbf17e6307d671ab13871a7e12f217e
Federico Cecchetto <fedecek3@gmail.com>
2020-12-11 12:39:02 +0100

    Fix formatting

--- src/client/game/dvars.cpp
+++ src/client/game/dvars.cpp
@@ -5,18 +5,18 @@
 
 namespace dvars
 {
-	game::dvar_t* con_inputBoxColor = nullptr;
-	game::dvar_t* con_inputHintBoxColor = nullptr;
-	game::dvar_t* con_outputBarColor = nullptr;
-	game::dvar_t* con_outputSliderColor = nullptr;
-	game::dvar_t* con_outputWindowColor = nullptr;
-	game::dvar_t* con_inputDvarMatchColor = nullptr;
-	game::dvar_t* con_inputDvarValueColor = nullptr;
-	game::dvar_t* con_inputDvarInactiveValueColor = nullptr;
-	game::dvar_t* con_inputCmdMatchColor = nullptr;
+    game::dvar_t* con_inputBoxColor = nullptr;
+    game::dvar_t* con_inputHintBoxColor = nullptr;
+    game::dvar_t* con_outputBarColor = nullptr;
+    game::dvar_t* con_outputSliderColor = nullptr;
+    game::dvar_t* con_outputWindowColor = nullptr;
+    game::dvar_t* con_inputDvarMatchColor = nullptr;
+    game::dvar_t* con_inputDvarValueColor = nullptr;
+    game::dvar_t* con_inputDvarInactiveValueColor = nullptr;
+    game::dvar_t* con_inputCmdMatchColor = nullptr;
 
-	game::dvar_t* g_gravity = nullptr;
-	game::dvar_t* g_speed = nullptr;
+    game::dvar_t* g_gravity = nullptr;
+    game::dvar_t* g_speed = nullptr;
 
     game::dvar_t* pm_bouncing = nullptr;
 
