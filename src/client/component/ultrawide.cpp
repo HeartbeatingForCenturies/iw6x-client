6f168bebdc1862e15f8e0901e5146a716391d173
momo5502 <mauriceheumann@gmail.com>
2020-12-13 21:17:52 +0100

    Format code

--- src/client/component/ultrawide.cpp
+++ src/client/component/ultrawide.cpp
@@ -9,9 +9,10 @@ namespace ultrawide
 {
 	namespace
 	{
-		game::dvar_t* dvar_register_aspect_ratio(const char* name, const char**, int defaultIndex, unsigned int flags, const char* description)
+		game::dvar_t* dvar_register_aspect_ratio(const char* name, const char**, int default_index, unsigned int flags,
+		                                         const char* description)
 		{
-			static std::vector < const char* > values =
+			static std::vector<const char*> values =
 			{
 				"auto",
 				"standard",
@@ -22,10 +23,12 @@ namespace ultrawide
 			};
 
 			// register custom aspect ratio dvar
-			dvars::r_aspectRatioCustom = game::Dvar_RegisterFloat("r_aspectRatioCustom", 16.0f / 9.0f, 4.0f / 3.0f, 63.0f / 9.0f, flags, "Screen aspect ratio. Divide width by height to get the aspect ratio value. For example: 21 / 9 = 2,33");
+			dvars::r_aspectRatioCustom = game::Dvar_RegisterFloat("r_aspectRatioCustom", 16.0f / 9.0f, 4.0f / 3.0f,
+			                                                      63.0f / 9.0f, flags,
+			                                                      "Screen aspect ratio. Divide width by height to get the aspect ratio value. For example: 21 / 9 = 2,33");
 
 			// register r_aspectRatio dvar
-			return game::Dvar_RegisterEnum(name, values.data(), defaultIndex, flags, description);
+			return game::Dvar_RegisterEnum(name, values.data(), default_index, flags, description);
 		}
 
 
@@ -41,8 +44,8 @@ namespace ultrawide
 
 		void __fastcall ultrawide_patch(wnd_struct* wnd)
 		{
-			const auto& r_aspectRatio = game::Dvar_FindVar("r_aspectRatio");
-			if (r_aspectRatio && r_aspectRatio->current.integer == 4 && dvars::r_aspectRatioCustom)
+			const auto& r_aspect_ratio = game::Dvar_FindVar("r_aspectRatio");
+			if (r_aspect_ratio && r_aspect_ratio->current.integer == 4 && dvars::r_aspectRatioCustom)
 			{
 				hud_aspect_ratio = dvars::r_aspectRatioCustom->current.value;
 				hud_aspect_ratio_inv = -hud_aspect_ratio;
@@ -87,13 +90,13 @@ namespace ultrawide
 			a.mov(eax, dword_ptr(reinterpret_cast<int64_t>(&hud_aspect_ratio)));
 			a.movd(xmm5, eax);
 
-			a.divss(xmm0, xmm5);  // divss   xmm0, cs:FLOAT_1_77778 // -> custom aspect ratio
-			a.subss(xmm1, xmm0);  // subss   xmm1, xmm0
+			a.divss(xmm0, xmm5); // divss   xmm0, cs:FLOAT_1_77778 // -> custom aspect ratio
+			a.subss(xmm1, xmm0); // subss   xmm1, xmm0
 			a.movaps(xmm0, xmm3); // movaps  xmm0, xmm3
 
 			a.mov(eax, dword_ptr(reinterpret_cast<int64_t>(&hud_aspect_ratio_inv)));
 			a.movd(xmm5, eax);
-			a.mulss(xmm0, xmm5);  // mulss   xmm0, cs:dword_14083D47C // -1.77778 // -> left menu offset
+			a.mulss(xmm0, xmm5); // mulss   xmm0, cs:dword_14083D47C // -1.77778 // -> left menu offset
 
 			a.pop(eax);
 
@@ -106,13 +109,13 @@ namespace ultrawide
 			a.mov(eax, dword_ptr(reinterpret_cast<int64_t>(&hud_aspect_ratio)));
 			a.movd(xmm5, eax);
 
-			a.divss(xmm0, xmm5);  // divss   xmm0, cs:FLOAT_1_77778 // -> custom aspect ratio
-			a.subss(xmm1, xmm0);  // subss   xmm1, xmm0
+			a.divss(xmm0, xmm5); // divss   xmm0, cs:FLOAT_1_77778 // -> custom aspect ratio
+			a.subss(xmm1, xmm0); // subss   xmm1, xmm0
 			a.movaps(xmm0, xmm3); // movaps  xmm0, xmm3
 
 			a.mov(eax, dword_ptr(reinterpret_cast<int64_t>(&hud_aspect_ratio_inv)));
 			a.movd(xmm5, eax);
-			a.mulss(xmm0, xmm5);  // mulss   xmm0, cs:dword_14083D47C // -1.77778 // -> left menu offset
+			a.mulss(xmm0, xmm5); // mulss   xmm0, cs:dword_14083D47C // -1.77778 // -> left menu offset
 
 			a.pop(eax);
 
@@ -127,8 +130,8 @@ namespace ultrawide
 			a.mov(eax, dword_ptr(reinterpret_cast<int64_t>(&hud_aspect_ratio)));
 			a.movd(xmm5, eax);
 
-			a.mulss(xmm0, xmm5);  // mulss   xmm0, cs:FLOAT_1_77778 // -> custom aspect ratio
-			a.mulss(xmm0, xmm1);  // mulss   xmm0, xmm1
+			a.mulss(xmm0, xmm5); // mulss   xmm0, cs:FLOAT_1_77778 // -> custom aspect ratio
+			a.mulss(xmm0, xmm1); // mulss   xmm0, xmm1
 
 			a.pop(eax);
 
@@ -141,8 +144,8 @@ namespace ultrawide
 			a.mov(eax, dword_ptr(reinterpret_cast<int64_t>(&hud_aspect_ratio)));
 			a.movd(xmm5, eax);
 
-			a.mulss(xmm0, xmm5);  // mulss   xmm0, cs:FLOAT_1_77778 // -> custom aspect ratio
-			a.mulss(xmm0, xmm1);  // mulss   xmm0, xmm1
+			a.mulss(xmm0, xmm5); // mulss   xmm0, cs:FLOAT_1_77778 // -> custom aspect ratio
+			a.mulss(xmm0, xmm1); // mulss   xmm0, xmm1
 
 			a.pop(eax);
 
@@ -186,28 +189,46 @@ namespace ultrawide
 		void post_unpack() override
 		{
 			// ultrawide patches
-			utils::hook::call(SELECT_VALUE(0x140510D0E, 0x1405DCF58), dvar_register_aspect_ratio); // register r_aspectRatioCustom
-			utils::hook::jump(SELECT_VALUE(0x140519EC7, 0x1405E68F7), SELECT_VALUE(ultrawide_spawn_window_stub_sp, ultrawide_spawn_window_stub_mp), true); // apply general aspect ratio
-			utils::hook::jump(SELECT_VALUE(0x1401CD75B, 0x1402008BB), SELECT_VALUE(ultrawide_menu_stub_01_sp, ultrawide_menu_stub_01_mp), true); // fix menu aspect ratio and left offset
-			utils::hook::jump(SELECT_VALUE(0x1401CD793, 0x1402008F3), SELECT_VALUE(ultrawide_menu_stub_02_sp, ultrawide_menu_stub_02_mp), true); // fix menu aspect ratio right offset
-			utils::hook::jump(SELECT_VALUE(0x14024D323, 0x1402F7043), SELECT_VALUE(ultrawide_hud_stub_sp, ultrawide_hud_stub_mp), true); // fix aspect ratio for loadscreen and general in-game hud
+			utils::hook::call(SELECT_VALUE(0x140510D0E, 0x1405DCF58), dvar_register_aspect_ratio);
+			// register r_aspectRatioCustom
+			utils::hook::jump(
+				SELECT_VALUE(0x140519EC7, 0x1405E68F7),
+				SELECT_VALUE(ultrawide_spawn_window_stub_sp, ultrawide_spawn_window_stub_mp), true);
+			// apply general aspect ratio
+			utils::hook::jump(
+				SELECT_VALUE(0x1401CD75B, 0x1402008BB),
+				SELECT_VALUE(ultrawide_menu_stub_01_sp, ultrawide_menu_stub_01_mp), true);
+			// fix menu aspect ratio and left offset
+			utils::hook::jump(
+				SELECT_VALUE(0x1401CD793, 0x1402008F3),
+				SELECT_VALUE(ultrawide_menu_stub_02_sp, ultrawide_menu_stub_02_mp), true);
+			// fix menu aspect ratio right offset
+			utils::hook::jump(
+				SELECT_VALUE(0x14024D323, 0x1402F7043), SELECT_VALUE(ultrawide_hud_stub_sp, ultrawide_hud_stub_mp),
+				true); // fix aspect ratio for loadscreen and general in-game hud
 
 			// safeArea_adjusted dvars :: disable resets (in-game hud padding)
 			utils::hook::nop(SELECT_VALUE(0x14024AEF2, 0x1402D4FB2), 5); // safeArea_adjusted_horizontal
 			utils::hook::nop(SELECT_VALUE(0x14024D655, 0x1402F7375), 5); // safeArea_adjusted_horizontal
 
-			utils::hook::nop(SELECT_VALUE(0x14024AF19, 0x1402D4FD9), 5); utils::hook::set<BYTE>(SELECT_VALUE(0x14024AF19, 0x1402D4FD9), 0xC3); // safeArea_adjusted_vertical :: return instead of jumping to Dvar_SetFloat 
+			utils::hook::nop(SELECT_VALUE(0x14024AF19, 0x1402D4FD9), 5);
+			utils::hook::set<BYTE>(SELECT_VALUE(0x14024AF19, 0x1402D4FD9), 0xC3);
+			// safeArea_adjusted_vertical :: return instead of jumping to Dvar_SetFloat
 			utils::hook::nop(SELECT_VALUE(0x14024D664, 0x1402F7384), 5); // safeArea_adjusted_vertical
 
 			// safeArea_ dvars :: remove cheat protection + add saved flag
-			utils::hook::set<BYTE>(SELECT_VALUE(0x14024D19F + 4, 0x1402F6EBF + 4), 0x1); // register safeArea_horizontal (mov dword ptr [rsp+20h], 4)
-			utils::hook::set<BYTE>(SELECT_VALUE(0x14024D1D7 + 4, 0x1402F6EF7 + 4), 0x1); // register safeArea_vertical   (mov dword ptr [rsp+20h], 4)
+			utils::hook::set<BYTE>(SELECT_VALUE(0x14024D19F + 4, 0x1402F6EBF + 4), 0x1);
+			// register safeArea_horizontal (mov dword ptr [rsp+20h], 4)
+			utils::hook::set<BYTE>(SELECT_VALUE(0x14024D1D7 + 4, 0x1402F6EF7 + 4), 0x1);
+			// register safeArea_vertical   (mov dword ptr [rsp+20h], 4)
 
 			utils::hook::set<BYTE>(SELECT_VALUE(0x14024D207 + 5, 0x1402F6F27 + 5), 0x0);
-			utils::hook::set<BYTE>(SELECT_VALUE(0x14024D207 + 4, 0x1402F6F27 + 4), 0x1); // register safeArea_adjusted_horizontal (mov dword ptr [rsp+20h], 2000h)
+			utils::hook::set<BYTE>(SELECT_VALUE(0x14024D207 + 4, 0x1402F6F27 + 4), 0x1);
+			// register safeArea_adjusted_horizontal (mov dword ptr [rsp+20h], 2000h)
 
 			utils::hook::set<BYTE>(SELECT_VALUE(0x14024D237 + 5, 0x1402F6F57 + 5), 0x0);
-			utils::hook::set<BYTE>(SELECT_VALUE(0x14024D237 + 4, 0x1402F6F57 + 4), 0x1); // register safeArea_adjusted_vertical   (mov dword ptr [rsp+20h], 2000h)
+			utils::hook::set<BYTE>(SELECT_VALUE(0x14024D237 + 4, 0x1402F6F57 + 4), 0x1);
+			// register safeArea_adjusted_vertical   (mov dword ptr [rsp+20h], 2000h)
 		}
 	};
 }
