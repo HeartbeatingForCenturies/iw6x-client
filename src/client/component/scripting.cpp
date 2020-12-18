b2a78abe485bd98afa1b6ea7f87ab2853c5ab2d9
momo5502 <mauriceheumann@gmail.com>
2020-12-18 22:01:01 +0100

    Allow precaching after pre_main

--- src/client/component/scripting.cpp
+++ src/client/component/scripting.cpp
@@ -52,6 +52,17 @@ namespace scripting
 		}
 	}
 
+	bool has_config_string_index(const unsigned int csIndex)
+	{
+		const auto* s_constantConfigStringTypes = reinterpret_cast<uint8_t*>(0x141721F80);
+		return csIndex < 0xDC4 && s_constantConfigStringTypes[csIndex] < 0x18u;
+	}
+
+	int is_pre_main_stub()
+	{
+		return game::CL_IsCgameInitialized();
+	}
+
 	class component final : public component_interface
 	{
 	public:
@@ -66,6 +77,16 @@ namespace scripting
 			{
 				lua::engine::run_frame();
 			}, scheduler::pipeline::server);
+
+			if(!game::environment::is_sp())
+			{
+				// Make some room for pre_main hook
+				utils::hook::jump(0x1402084A0, has_config_string_index);
+
+				// Allow precaching anytime
+				utils::hook::jump(0x1402084A5, is_pre_main_stub);
+				utils::hook::set<uint16_t>(0x1402084D0, 0xD3EB); // jump to 0x1402084A5
+			}
 		}
 	};
 }
