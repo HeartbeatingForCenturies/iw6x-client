d28eb1e009d1ba8b8d6b9f93f815346f8de3fbaf
Brentdevent <brentdevent@hotmail.com>
2021-07-30 21:40:11 +0200

    fix fetching bot names

--- src/client/component/network.hpp
+++ src/client/component/network.hpp
@@ -6,8 +6,8 @@ namespace network
 	using callback = std::function<void(const game::netadr_s&, const std::string_view&)>;
 
 	void on(const std::string& command, const callback& callback);
-	void send(const game::netadr_s& address, const std::string& command, const std::string& data, char separator = ' ');
-	void send(const game::netadr_s& address, const std::string& data);
+	void send(const game::netadr_s& address, const std::string& command, const std::string& data = {}, char separator = ' ');
+	void send_data(const game::netadr_s& address, const std::string& data);
 
 	bool are_addresses_equal(const game::netadr_s& a, const game::netadr_s& b);
 
